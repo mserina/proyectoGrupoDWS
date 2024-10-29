e89cefe72cfb5adabffce1ab55950ae75f6f7840
DESKTOP-1CSI9JF\Alumno <dragontero2@gmail.com>
2024-10-29 13:31:33 +0100

    no message

---
+++ src/main/java/edu/msm/proyectoGrupoDWS/ProyectoGrupoDwsApplication.java
@@ -0,0 +1,13 @@
+package edu.msm.proyectoGrupoDWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGrupoDwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGrupoDwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/msm/proyectoGrupoDWS/ProyectoGrupoDwsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.msm.proyectoGrupoDWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGrupoDwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
